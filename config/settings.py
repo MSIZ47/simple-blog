24dfb0cd19e6e6538e0a0f64290cc29b21ec8a2d
Mahdi Safdarian <msiz47king@gmail.com>
2024-07-22 12:43:25 +0330

    registration complete

--- config/settings.py
+++ config/settings.py
@@ -126,3 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'post_list'
 LOGOUT_REDIRECT_URL = 'post_list'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
